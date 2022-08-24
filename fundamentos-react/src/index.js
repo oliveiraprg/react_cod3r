f05904a1afc4928a333367143685d5bafea6076c
Gabriel Alves Oliveira <111614350+oliveiraprg@users.noreply.github.com>
2022-08-24 03:53:55 -0300

    creating first component

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,9 +2,12 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import './index.css'
 
-const tag = <strong>Olá React!</strong>
+import Primeiro from './components/basicos/Primeiro'
+
 
 ReactDOM.render(
-    tag,
+    <div>
+        <Primeiro></Primeiro>
+    </div>,
     document.getElementById('root')
 )
