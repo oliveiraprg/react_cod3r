01438b7e370ebe7187a499ad46f92075a85b75ec
Gabriel Alves Oliveira <111614350+oliveiraprg@users.noreply.github.com>
2022-08-24 04:20:29 -0300

    passing parameters to a component

---
+++ fundamentos-react/src/components/basicos/ComParametro.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function ComParametro (props) {
+    const title = props.titulo
+    const aluno = props.aluno
+    const nota = props.nota
+    const situacao = nota > 7 ?  'Aprovado' : 'Recuperação'
+
+    return (
+        <div>
+            <h2>{title}</h2>
+            <p>{aluno} tem nota {nota}</p>
+            <p>Foi {situacao}</p>
+        </div>
+    )
+}
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,11 +3,21 @@ import React from 'react'
 import './index.css'
 
 import Primeiro from './components/basicos/Primeiro'
-
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDOM.render(
     <div>
-        <Primeiro></Primeiro>
+        <Primeiro />
+        <ComParametro 
+            titulo='Situação do aluno(a)' 
+            aluno='Pedro'
+            nota={6.3}
+        />
+        <ComParametro 
+            titulo='Situação do aluno(a)' 
+            aluno='Maria'
+            nota={9.9}
+        />
     </div>,
     document.getElementById('root')
 )
