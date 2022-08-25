b3d4da5c3b26e788853d076a6762e12eb205882b
Gabriel Alves Oliveira <111614350+oliveiraprg@users.noreply.github.com>
2022-08-25 10:20:29 -0300

    refactoring the look of the application

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Card from './components/layout/Card'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
@@ -10,19 +11,32 @@ export default function App(props) {
     return (    
         <div id='app'>
             <h1>Fundamentos React</h1>
-            <Aleatorio min={1} max={10}/>
-            <Fragmento />
-            <ComParametro
-                titulo='Situação do aluno(a)'
-                aluno='Pedro'
-                nota={6.3}
-            />
-            <ComParametro
+            <Card titulo="#04 - Desafio Aleatorio entre 2 numeros">
+                <Aleatorio min={1} max={60}/>
+                <Aleatorio min={1} max={60}/>
+                <Aleatorio min={1} max={60}/>
+                <Aleatorio min={1} max={60}/>
+                <Aleatorio min={1} max={60}/>
+                <Aleatorio min={1} max={60}/>
+            </Card>
+            <Card titulo="#03 - Fragmento">
+                <Fragmento />
+            </Card>
+            <Card titulo="#02 - Componente com paramentro">
+                <ComParametro
+                    titulo='Situação do aluno(a)'
+                    aluno='Pedro'
+                    nota={6.3}
+                />
+                <ComParametro
                 titulo='Situação do aluno(a)'
                 aluno='Maria'
                 nota={9.9}
-            />
-            <Primeiro />
+                />
+            </Card>
+            <Card titulo="#01 - Primeiro Componente">
+                <Primeiro />
+            </Card>
         </div>
     )
 }
--- fundamentos-react/src/components/basicos/Aleatorio.jsx
+++ fundamentos-react/src/components/basicos/Aleatorio.jsx
@@ -6,7 +6,6 @@ export default function Aleatorio(props) {
 
     return (
         <div>
-            <h2>Numero aleatorio entre dois valores</h2>
             <p>Menor valor {min} maior valor {max}</p>
             <p>Resultado {aleatorio}</p>
         </div>
