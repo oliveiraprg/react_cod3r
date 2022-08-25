0149c40cce6fe174b7aa96adaf1e6e3dcb9f8c33
Gabriel Alves Oliveira <111614350+oliveiraprg@users.noreply.github.com>
2022-08-25 12:43:46 -0300

    styling components

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -12,20 +12,20 @@ export default function App(props) {
         <div className='App'>
             <h1>Fundamentos React</h1>
             <div className="Cards">
-                <Card titulo="#04 - Desafio Aleatorio entre 2 numeros">
+                <Card titulo="#04 - Desafio Aleatorio" color="#00BFFF">
                     <Aleatorio min={1} max={60}/>
                 </Card>
-                <Card titulo="#03 - Fragmento">
+                <Card titulo="#03 - Fragmento" color="#F08080">
                     <Fragmento />
                 </Card>
-                <Card titulo="#02 - Componente com paramentro">
+                <Card titulo="#02 - Componente com paramentro" color="#3CB371">
                     <ComParametro
                     titulo='Situação do aluno(a)'
                     aluno='Maria'
                     nota={9.9}
                     />
                 </Card>
-                <Card titulo="#01 - Primeiro Componente">
+                <Card titulo="#01 - Primeiro Componente" color="#BDB76B">
                     <Primeiro />
                 </Card>
             </div>
